6c231f2105772581a6460e15c2052e5395db82ac
Malik05255 <mmbbrr99@gmail.com>
2026-08-23 17:30:17 +0300

    Update ChatDatabaseV2.kt

--- app/src/main/kotlin/com/vibe/app/data/database/ChatDatabaseV2.kt
+++ app/src/main/kotlin/com/vibe/app/data/database/ChatDatabaseV2.kt
@@ -15,18 +15,33 @@ import com.vibe.app.data.database.entity.PlatformV2
 import com.vibe.app.data.database.entity.Project
 import com.vibe.app.data.database.entity.ProjectBuildStatusConverter
 import com.vibe.app.data.database.entity.StringListConverter
+import com.vibe.app.data.database.entity.ClientTypeConverter
 
 @Database(
-    entities = [ChatRoomV2::class, MessageV2::class, PlatformV2::class, ChatPlatformModelV2::class, Project::class],
+    entities = [
+        ChatRoomV2::class,
+        MessageV2::class,
+        PlatformV2::class,
+        ChatPlatformModelV2::class,
+        Project::class
+    ],
     version = 3,
     exportSchema = true,
 )
-@TypeConverters(StringListConverter::class, ProjectBuildStatusConverter::class)
+@TypeConverters(
+    StringListConverter::class,
+    ProjectBuildStatusConverter::class,
+    ClientTypeConverter::class
+)
 abstract class ChatDatabaseV2 : RoomDatabase() {
 
     abstract fun platformDao(): PlatformV2Dao
+
     abstract fun chatRoomDao(): ChatRoomV2Dao
+
     abstract fun messageDao(): MessageV2Dao
+
     abstract fun chatPlatformModelDao(): ChatPlatformModelV2Dao
+
     abstract fun projectDao(): ProjectDao
 }

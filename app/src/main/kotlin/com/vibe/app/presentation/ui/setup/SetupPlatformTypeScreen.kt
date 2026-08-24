acd11fb2103a3942df0c76112e6431cb41972798
Malik05255 <mmbbrr99@gmail.com>
2026-08-24 20:59:39 +0300

    Update SetupPlatformTypeScreen.kt

--- app/src/main/kotlin/com/vibe/app/presentation/ui/setup/SetupPlatformTypeScreen.kt
+++ app/src/main/kotlin/com/vibe/app/presentation/ui/setup/SetupPlatformTypeScreen.kt
@@ -3,6 +3,7 @@ package com.vibe.app.presentation.ui.setup
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,7 +11,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape

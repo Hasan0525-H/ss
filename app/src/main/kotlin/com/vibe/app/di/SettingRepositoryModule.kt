0138ee7956ea8d6f44f334de8a6add45c3804cb2
Malik05255 <mmbbrr99@gmail.com>
2026-08-24 23:29:40 +0300

    Update SettingRepositoryModule.kt

--- app/src/main/kotlin/com/vibe/app/di/SettingRepositoryModule.kt
+++ app/src/main/kotlin/com/vibe/app/di/SettingRepositoryModule.kt
@@ -1,14 +1,15 @@
 package com.vibe.app.di
 
+import com.vibe.app.data.database.dao.ChatPlatformModelV2Dao
+import com.vibe.app.data.database.dao.PlatformV2Dao
+import com.vibe.app.data.datastore.SettingDataSource
+import com.vibe.app.data.network.OpenRouterModelsAPI
+import com.vibe.app.data.repository.SettingRepository
+import com.vibe.app.data.repository.SettingRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import com.vibe.app.data.database.dao.ChatPlatformModelV2Dao
-import com.vibe.app.data.database.dao.PlatformV2Dao
-import com.vibe.app.data.datastore.SettingDataSource
-import com.vibe.app.data.repository.SettingRepository
-import com.vibe.app.data.repository.SettingRepositoryImpl
 import javax.inject.Singleton
 
 @Module
@@ -20,6 +21,12 @@ object SettingRepositoryModule {
     fun provideSettingRepository(
         settingDataSource: SettingDataSource,
         platformV2Dao: PlatformV2Dao,
-        chatPlatformModelV2Dao: ChatPlatformModelV2Dao
-    ): SettingRepository = SettingRepositoryImpl(settingDataSource, platformV2Dao, chatPlatformModelV2Dao)
+        chatPlatformModelV2Dao: ChatPlatformModelV2Dao,
+        openRouterModelsAPI: OpenRouterModelsAPI
+    ): SettingRepository = SettingRepositoryImpl(
+        settingDataSource,
+        platformV2Dao,
+        chatPlatformModelV2Dao,
+        openRouterModelsAPI
+    )
 }

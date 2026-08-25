a466edf4192109317c200be8996c0748727785a5
Malik05255 <mmbbrr99@gmail.com>
2026-08-25 16:55:10 +0300

    Update LanguageViewModel.kt

--- app/src/main/kotlin/com/vibe/app/presentation/ui/setting/LanguageViewModel.kt
+++ app/src/main/kotlin/com/vibe/app/presentation/ui/setting/LanguageViewModel.kt
@@ -5,25 +5,19 @@ import com.vibe.app.data.preferences.LanguageManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
-
 @HiltViewModel
 class LanguageViewModel @Inject constructor(
     private val languageManager: LanguageManager
 ) : ViewModel() {
 
+    val language = languageManager.language
 
-    val language =
-        languageManager.language
-
-
-
-    fun changeLanguage(
-        language: String
-    ) {
-
-        languageManager
-            .setLanguage(language)
-
+    fun changeLanguage(language: String) {
+        languageManager.setLanguage(language)
     }
 
+    // إضافة هذه الدالة لتتوافق مع الاستدعاءات الموجودة في الواجهات (SettingScreen / LanguageDialog)
+    fun setLanguage(language: String) {
+        languageManager.setLanguage(language)
+    }
 }
